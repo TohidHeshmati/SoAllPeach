07d8e428ad9a9bcdfc15eb48e6b9e20b648dfc09
Tohid Heshmati <[Theshmati@justaddmusic.net](mailto:Theshmati@justaddmusic.net)>
2020-03-31 20:24:26 +0200

    bad commit msg

--- prime/src/main.rs
+++ prime/src/main.rs
@@ -1,26 +1,22 @@
 extern crate prime;
 use std::fs;
-fn main() {
-    let number: i64 = 13;
-    if prime::is_prime(number) {
-        println!("is prime")
-    } else {
-        println!("is not prime")
-    }
+use std::*;
 
-    let data = fs::read_to_string("test.txt").expect("something is wrong");
+fn main() {
+    let args:Vec<String> = env::args().collect();
+    let data = fs::read_to_string(&args[1]).expect("something is wrong");
     let data = data.split("\n");
+
     for num in data {
-        //let n = num.parse::<i64>();
         match num.parse::<i64>() {
             Ok(n) => {
                 if prime::is_prime(n) {
-                    println!("is prime")
+                    println!("1")
                 } else {
-                    println!("is not prime")
+                    println!("0")
                 }
             }
-            _ => println!("finish"),
+            _ => continue,
         }
     }
 }
